9d233cb407ac1709d5021ff968393a79f2c4a7f5
Simon Biggs <me@simonbiggs.net>
2019-05-23 22:12:22 +1000

    add vendored wheels

---
+++ wheels/create_json_index.py
@@ -0,0 +1,16 @@
+import json
+from uuid import uuid5, UUID
+from glob import glob
+
+def main():
+    wheels = tuple(glob('*.whl'))
+    a_uuid = uuid5(
+        UUID('84240474-7d53-11e9-af91-74d02b7a0098'), 
+        repr(wheels))
+    filename = "index-{}.json".format(a_uuid)
+
+    with open(filename, 'w') as a_file:
+        json.dump(wheels, a_file)
+
+if __name__ == "__main__":
+    main()
