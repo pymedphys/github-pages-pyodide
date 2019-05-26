609d2fdb52047409cc7264ea342e43fe517cc0dc
Simon Biggs <me@simonbiggs.net>
2019-05-26 20:24:14 +1000

    add a blacklist, use this to allow removing packages from new distros without removing old copies

--- wheels/create_json_index.py
+++ wheels/create_json_index.py
@@ -2,15 +2,23 @@ import json
 from uuid import uuid5, UUID
 from glob import glob
 
+
 def main():
-    wheels = tuple(glob('*.whl'))
+    with open('blacklist.json', 'r') as a_file:
+        blacklist = json.load(a_file)
+
+    wheels = tuple([
+        wheel
+        for wheel in glob('*.whl')
+        if wheel not in blacklist])
     a_uuid = uuid5(
-        UUID('84240474-7d53-11e9-af91-74d02b7a0098'), 
+        UUID('84240474-7d53-11e9-af91-74d02b7a0098'),
         repr(wheels))
     filename = "index-{}.json".format(a_uuid)
 
     with open(filename, 'w') as a_file:
         json.dump(wheels, a_file)
+
 
 if __name__ == "__main__":
     main()
